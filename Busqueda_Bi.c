707f2ebfcb8b4b8bd2698af9bdf2c73f10d7a3af
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-10-13 12:06:18 -0600

    Quick Change

--- Busqueda_Bi.c
+++ Busqueda_Bi.c
@@ -1,16 +1,3 @@
-/**
- * @file first_mpi.c
- * @author Raul Jimenez
- * @author Bryann Alfaro
- * @author Donaldo Garcia
- * @brief First extra implementation of brute force improving performance
- * @version 0.1
- * @date 2023-05-09
- *
- * @copyright Copyright (c) 2023
- *
- */
-// Reference: Barlas Capitulo 5
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,18 +5,12 @@
 #include <unistd.h>
 #include <openssl/des.h>
 
-/**
- * @brief Read a file into a buffer
- * @param filename
- * @return char*
- */
 char *readFile(const char *filename)
 {
   FILE *fp;
   char *buffer;
   long fileSize;
 
-  // Open file for reading in binary mode
   fp = fopen(filename, "rb");
   if (fp == NULL)
   {
@@ -37,12 +18,10 @@ char *readFile(const char *filename)
     return NULL;
   }
 
-  // Get file size
   fseek(fp, 0L, SEEK_END);
   fileSize = ftell(fp);
   rewind(fp);
 
-  // Allocate memory for buffer
   buffer = (char *)malloc(fileSize + 1);
   if (buffer == NULL)
   {
@@ -51,7 +30,6 @@ char *readFile(const char *filename)
     return NULL;
   }
 
-  // Read file contents into buffer
   if (fread(buffer, fileSize, 1, fp) != 1)
   {
     fclose(fp);
@@ -60,25 +38,13 @@ char *readFile(const char *filename)
     return NULL;
   }
 
-  // Null-terminate the buffer
   buffer[fileSize] = '\0';
-
-  // Close file and return buffer
   fclose(fp);
   return buffer;
 }
 
-/**
- * @brief decrypt a text given a key
- * @param key key to decrypt
- * @param ciph text to decrypt
- * @param len length of the text
- *
- * @return void
- */
 void decrypt(long key, char *ciph, int len)
 {
-  // Set parity of key
   long k = 0;
   for (int i = 0; i < 8; ++i)
   {
@@ -86,52 +52,36 @@ void decrypt(long key, char *ciph, int len)
     k += (key & (0xFE << i * 8));
   }
 
-  // Initialize DES key
   DES_cblock desKey;
   memcpy(desKey, &k, sizeof(k));
   DES_set_odd_parity(&desKey);
 
-  // Initialize key schedule
   DES_key_schedule keySchedule;
   DES_set_key_unchecked(&desKey, &keySchedule);
 
-  // Decrypt the message using ECB mode
   for (size_t i = 0; i < len; i += 8)
   {
     DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &keySchedule, DES_DECRYPT);
   }
 
-  // Check padding
   size_t padLen = ciph[len - 1];
   if (padLen > 8)
   {
-    // Error: Invalid padding
     return;
   }
   for (size_t i = len - padLen; i < len; i++)
   {
     if (ciph[i] != padLen)
     {
-      // Error: Invalid padding
       return;
     }
   }
 
-  // Null-terminate the plaintext
   ciph[len - padLen] = '\0';
 }
 
-/**
- * @brief encrypt a text given a key
- * @param key key to encrypt
- * @param ciph text to encrypt
- * @param len length of the text
- *
- * @return void
- */
 void encryptText(long key, char *ciph, int len)
 {
-  // Set parity of key
   long k = 0;
   for (int i = 0; i < 8; ++i)
   {
@@ -139,49 +89,35 @@ void encryptText(long key, char *ciph, int len)
     k += (key & (0xFE << i * 8));
   }
 
-  // Initialize DES key
   DES_cblock desKey;
   memcpy(desKey, &k, sizeof(k));
   DES_set_odd_parity(&desKey);
 
-  // Initialize key schedule
   DES_key_schedule keySchedule;
   DES_set_key_unchecked(&desKey, &keySchedule);
 
-  // Calculate padding length
   size_t padLen = 8 - len % 8;
   if (padLen == 0)
   {
     padLen = 8;
   }
 
-  // Add padding to message
   for (size_t i = len; i < len + padLen; i++)
   {
     ciph[i] = padLen;
   }
 
-  // Encrypt the message using ECB mode
   for (size_t i = 0; i < len + padLen; i += 8)
   {
     DES_ecb_encrypt((DES_cblock *)(ciph + i), (DES_cblock *)(ciph + i), &keySchedule, DES_ENCRYPT);
   }
 }
 
-// key word to search in decrypted text to determine if the code was broken
 char search[] = "es una prueba de";
 
-/**
- * @brief try a key to decrypt a text
- * @param key key to try
- * @param ciph text to decrypt
- * @param len length of the text
- *
- * @return int 1 if the key is correct, 0 otherwise
- */
 int tryKey(long key, char *ciph, int len)
 {
-  char temp[len + 1]; //+1 because of the terminal character
+  char temp[len + 1];
   strcpy(temp, ciph);
   decrypt(key, temp, len);
   return strstr((char *)temp, search) != NULL;
@@ -195,7 +131,7 @@ int main(int argc, char *argv[])
   {
     return 1;
   }
-  theKey = strtol(argv[1], NULL, 10); // getting the key from the command line
+  theKey = strtol(argv[1], NULL, 10);
 
   int N, rank;
   long upper = (1L << 56);
@@ -216,7 +152,6 @@ int main(int argc, char *argv[])
   int flag;
   int ciphLen = strlen((char *)text);
 
-  // encrypt the text
   char cipher[ciphLen + 1];
   memcpy(cipher, text, ciphLen);
   cipher[ciphLen] = 0;
@@ -224,12 +159,11 @@ int main(int argc, char *argv[])
   encryptText(theKey, cipher, ciphLen);
   printf("Cipher text: %s\n", cipher);
 
-  // MPI
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &N);
 
-  start = MPI_Wtime(); // start time
+  start = MPI_Wtime();
   MPI_Irecv((void *)&realKey, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
   int iterCount = 0;
 
@@ -248,7 +182,7 @@ int main(int argc, char *argv[])
       }
       break;
     }
-    if (++iterCount % 1000 == 0) // test every 1000 iterations
+    if (++iterCount % 1000 == 0)
     {
       MPI_Test(&req, &flag, &st);
       if (flag)
@@ -259,13 +193,12 @@ int main(int argc, char *argv[])
 
   if (rank == 0)
   {
-    MPI_Wait(&req, &st); //  in case process 0 finishes  before the key  is  found
+    MPI_Wait(&req, &st);
     decrypt(realKey, (char *)cipher, ciphLen);
     cipher[ciphLen + 1] = '\0';
     printf("%li %s \n", realKey, cipher);
     printf("Time %f", end - start);
   }
 
-  // End MPi
   MPI_Finalize();
 }
